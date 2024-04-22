a52fedd380a7a3f8e2eb42efe083cef688c74ce2
fyxu <phyang0106@qq.com>
2024-04-22 19:29:17 +0800

    0.1A is a good choice

--- GPUMD/Pertube.py
+++ GPUMD/Pertube.py
@@ -11,8 +11,8 @@ import dpdata
 perturbed_system = dpdata.System("./POSCAR").perturb(
     pert_num=3,
     cell_pert_fraction=0.05,
-    atom_pert_distance=0.06,
+    atom_pert_distance=0.1,
     atom_pert_style="uniform",
-)    # uniform or constant is good，try not to use normal。
+)    # uniform or constant is good，try not to use normal. atom_pert_distance=0.1 means the coordinates are perturbed 0.1 Angstrom, which is a typical choice.
 perturbed_system.to_vasp_poscar('./POSCAR-perturb')
 #print(perturbed_system.data)
