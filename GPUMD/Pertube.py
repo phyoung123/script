35bcd6d59f26591a27c7e75d88f51b87492f418a
fyxu <phyang0106@qq.com>
2024-04-22 20:53:54 +0800

    含有OH键时坐标微扰不宜过大

--- GPUMD/Pertube.py
+++ GPUMD/Pertube.py
@@ -16,3 +16,5 @@ perturbed_system = dpdata.System("./POSCAR").perturb(
 )    # uniform or constant is good，try not to use normal. atom_pert_distance=0.1 means the coordinates are perturbed 0.1 Angstrom, which is a typical choice.
 perturbed_system.to_vasp_poscar('./POSCAR-perturb')
 #print(perturbed_system.data)
+
+# 体系中有O-H 键时要注意坐标微扰不宜过大，最好不要超过 0.1A，最好直接0.05A。
