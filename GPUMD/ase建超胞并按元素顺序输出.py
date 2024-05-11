ee7d74f917b3d113f30c518abc31d6d7d6fa2bdb
fyxu <phyang0106@qq.com>
2024-05-11 16:16:06 +0800

    update

--- GPUMD/ase建超胞并按元素顺序输出.py
+++ GPUMD/ase建超胞并按元素顺序输出.py
@@ -19,3 +19,24 @@ gnr.sort_atoms(sort_key='type', order=['Ca', 'Si', 'O'])
 
 write('POSCAR', gnr, direct=True)
 #写成VASP格式，然后用   VASP-poscar2lammps.awk  将其转换成lammps的data文件
+
+
+
+##################### 修改晶格常数，并且缩放原子位置#################
+from ase.io import read, write
+
+atoms = read('CONTCAR', format='vasp')
+print(atoms.cell[0,0])
+# atoms.cell[0] = [13.3109, 0, 0]
+# atoms.cell[1] = [ 0,13.3109, 0]
+# atoms.cell[2] = [ 0, 0,13.3109]
+new_length = 13.3109
+new_cell = atoms.cell.copy()
+new_cell[0] = [new_length, 0, 0 ]
+new_cell[1] = [0, new_length, 0]
+new_cell[2] = [0, 0, new_length]
+atoms.set_cell(new_cell, scale_atoms=True)
+atoms.wrap()
+
+write("POSCA-ovito.vasp", atoms, direct=True)
+
