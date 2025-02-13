43a0605e9da475f68ea178fc869ffe0d028926fe
fyxu <phyang0106@qq.com>
2025-02-13 21:50:26 +0800

    add file

---
+++ GPUMD/计算MAE和R平方.py
@@ -0,0 +1,20 @@
+import numpy as np  
+from sklearn.metrics import r2_score
+
+# MAE: Mean Absolute Error (平均绝对误差)， 它是衡量预测模型准确性的一种常用指标。MAE 是实际值与预测值之间绝对误差的平均值
+# R^2: 决定系数，是用来衡量回归模型拟合优度的一个指标
+
+def cal_MAE(y_true, y_pred):
+	delta_y = abs(y_true - y_pred)
+	n = len(y_true)
+	total = np.sum(delta_y)
+	MAE = total / n 
+	return MAE
+
+def cal_r2(y_true, y_pred):
+    y_mean = np.mean(y_true)
+    sst = np.sum((y_true - y_mean) ** 2)  # 总平方和
+    sse = np.sum((y_true - y_pred) ** 2)  # 残差平方和
+    r2 = 1 - (sse / sst)
+    return r2
+
