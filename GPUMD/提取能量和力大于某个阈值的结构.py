caad61bf51c0b7906eaa682181c2b0df920f8029
fyxu <phyang0106@qq.com>
2025-03-03 11:17:01 +0800

    update

--- GPUMD/提取能量和力大于某个阈值的结构.py
+++ GPUMD/提取能量和力大于某个阈值的结构.py
@@ -73,3 +73,41 @@ union_element = set(structure_indices_e).union(set(structure_indices_f))
 union_set = sorted(list(union_element))
 for idx in union_set:
     write('select.xyz', atoms[idx], append=True)
+
+
+#------------貌似上面的不行----------------
+import numpy as np
+from ase.io import read, write
+
+atoms = read('train.xyz', index=":")  # 读取FPS抽取到的结构并重新做的DFT计算
+
+file_f = 'force_train.out'
+force = np.loadtxt(file_f)
+file_e = 'energy_train.out'
+energy = np.loadtxt(file_e)
+
+energy_diff = energy[:, 1] - energy[:, 0]
+energy_diff = energy_diff * 1000
+
+N = 96  # number of atoms
+
+# 计算力的差值
+forces_diff = force[:, 3:6] - force[:, 0:3]
+add_idx = []
+testing_idx = []
+
+# 将数据重新整形以便按结构分组
+forces_diff_reshaped = forces_diff.reshape(-1, N, 3)
+# print(forces_diff_reshaped[0])
+for idx in range(len(forces_diff_reshaped)):
+    rmse_f = np.sqrt(np.mean(forces_diff_reshaped[idx] ** 2))
+    rmse_e = np.sqrt(np.mean(energy_diff[idx] ** 2))
+    if rmse_f > 0.1 or rmse_e > 3:
+        add_idx.append(idx)
+    else:
+        testing_idx.append(idx)
+
+for idx in add_idx:
+    write('select.xyz', atoms[idx], append=True)
+for idx in testing_idx:
+    write('test.xyz', atoms[idx], append=True)
